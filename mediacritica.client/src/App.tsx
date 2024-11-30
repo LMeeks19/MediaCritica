ed9f151f7a9a7a64427101eff90ea0f08ddd50df
Louie <louiemeeks@icloud.com>
2024-11-30 22:26:46 +0000

    Added API Call to Movie/TV database
    This was to test the response data

--- mediacritica.client/src/App.tsx
+++ mediacritica.client/src/App.tsx
@@ -1,7 +1,19 @@
-import './App.css';
+import { useEffect } from "react";
+import "./App.css";
+
+const APIKEY = "MY API KEY";
 
 function App() {
 
+  useEffect (() => {
+    async function FetchMovieData() {
+      var media = await fetch(`https://www.omdbapi.com/?i=tt11126994&season=2&episode=1&apikey=${APIKEY}`).then((response) => response.json());
+      console.log(media);
+    }
+    FetchMovieData();
+  }, [])
+
+  return <h1>Media Critica</h1>;
 }
 
 export default App;
