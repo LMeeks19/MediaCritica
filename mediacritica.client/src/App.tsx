3915414f3877cf4aa95109a170e35caef02a3934
Louie <louiemeeks@icloud.com>
2024-11-30 23:57:12 +0000

    Added .env file for sensitive data
    Stores API keys that are not commited to Git

--- mediacritica.client/src/App.tsx
+++ mediacritica.client/src/App.tsx
@@ -1,13 +1,16 @@
 import { useEffect } from "react";
 import "./App.css";
+import * as dotenv from "dotenv";
 
-const APIKEY = "MY API KEY";
+dotenv.config();
+
+const MEDIA_API_KEY = process.env.MEDIA_API_KEY;
 
 function App() {
 
   useEffect (() => {
     async function FetchMovieData() {
-      var media = await fetch(`https://www.omdbapi.com/?i=tt11126994&season=2&episode=1&apikey=${APIKEY}`).then((response) => response.json());
+      var media = await fetch(`https://www.omdbapi.com/?i=tt11126994&season=2&episode=1&apikey=${MEDIA_API_KEY}`).then((response) => response.json());
       console.log(media);
     }
     FetchMovieData();
