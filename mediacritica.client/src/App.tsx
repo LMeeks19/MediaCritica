9f5e3a2892dbb90e9813694061335ac8e4ef1a61
Louie <louiemeeks@icloud.com>
2024-12-02 03:45:19 +0000

    Completed the home page

--- mediacritica.client/src/App.tsx
+++ mediacritica.client/src/App.tsx
@@ -1,6 +1,6 @@
 import { RouterProvider } from "react-router-dom";
 import { router } from "./Router/Router";
-import "./App.css";
+import "./App.scss";
 
 function App() {
   return (
--- mediacritica.client/src/Pages/HomePage.tsx
+++ mediacritica.client/src/Pages/HomePage.tsx
@@ -1,34 +1,56 @@
 import { useEffect, useState } from "react";
-import "../Style/HomePage.css";
 import { MediaSearchModel } from "../Interfaces/MediaSearchModel";
 import { MediaSearchResponse } from "../Interfaces/MediaSearchResponse";
 import {
   CapitaliseFirstLetter,
   MediaYearFormatter,
 } from "../Helpers/StringHelper";
+import { BeatLoader } from "react-spinners";
+import $ from "jquery";
+import { AutoTextSize } from "auto-text-size";
+import "../Style/HomePage.scss";
 
 function HomePage() {
   const mediaServiceApiKey = import.meta.env.VITE_SERVICE_API_KEY;
-
+  const [isLoading, setIsLoading] = useState<boolean>(true);
   const [searchTerm, setSerarchTerm] = useState<string>("");
   const [mediaSearchResults, setMediaSearchResults] = useState<
     MediaSearchModel[]
   >([] as MediaSearchModel[]);
 
   useEffect(() => {
+    setIsLoading(true);
     const timeout = setTimeout(async () => {
       var mediaSearchResponse = (await fetch(
         `https://www.omdbapi.com/?s=${searchTerm}&apikey=${mediaServiceApiKey}`
       ).then((response) => response.json())) as MediaSearchResponse;
       setMediaSearchResults(mediaSearchResponse.Search ?? []);
+      setIsLoading(false);
     }, 1000);
-
     return () => clearTimeout(timeout);
   }, [searchTerm]);
 
+  function scrollTitleIfOverflowing(id: string) {
+    let element = $(`#${id}`);
+    let info = element.find(".homepage-result-details");
+    let title = info.find(".result-details-title");
+    title.stop();
+    title.animate({ scrollLeft: 1000 }, 8000, function () {});
+  }
+
+  function unscrollTitleIfOverflowing(id: string) {
+    let element = $(`#${id}`);
+    let info = element.find(".homepage-result-details");
+    let title = info.find(".result-details-title");
+    title.stop();
+    title.animate({ scrollLeft: -1000 }, 8000, function () {});
+  }
+
   return (
     <div className="homepage-container">
-      <div className="homepage-title">MEDIA CRITICA</div>
+      <div className="homepage-title">
+        <AutoTextSize mode="multiline">MEDIA CRITICA</AutoTextSize>
+      </div>
       <div className="homepage-searchbar">
         <input
           className="homepage-input"
@@ -47,29 +69,62 @@ function HomePage() {
           <i className="fa-solid fa-magnifying-glass fa-xl fa-flip-horizontal" />
         </div>
       </div>
-      <div className="homepage-results">
-        {mediaSearchResults.map((mediaSearchResult) => {
-          return (
-            <div className="homepage-result" key={mediaSearchResult.imdbID}>
-              <img
-                className="homepage-result-image"
-                src={mediaSearchResult.Poster}
-              ></img>
-              <div className="homepage-result-details">
-                <div className="result-details-title">
-                  {mediaSearchResult.Title}
-                </div>
-                <div className="result-details-type">
-                  {CapitaliseFirstLetter(mediaSearchResult.Type)}
-                </div>
-                <div className="result-details-year">
-                  {MediaYearFormatter(mediaSearchResult.Year)}
+      {mediaSearchResults.length > 0 ? (
+        <div className="homepage-results">
+          {mediaSearchResults.map((mediaSearchResult) => {
+            return (
+              <div
+                className="homepage-result"
+                id={mediaSearchResult.imdbID}
+                key={mediaSearchResult.imdbID}
+                onMouseOver={() =>
+                  scrollTitleIfOverflowing(mediaSearchResult.imdbID)
+                }
+                onMouseOut={() =>
+                  unscrollTitleIfOverflowing(mediaSearchResult.imdbID)
+                }
+              >
+                {mediaSearchResult.Poster !== "N/A" ? (
+                  <img
+                    draggable="false"
+                    className="homepage-result-image"
+                    src={mediaSearchResult.Poster}
+                  ></img>
+                ) : (
+                  <div className="homepage-result-image empty">
+                    <i className="fa-regular fa-image"></i>
+                  </div>
+                )}
+                <div className="homepage-result-details">
+                  <div className="result-details-title">
+                    {mediaSearchResult.Title}
+                  </div>
+                  <div className="result-sub-details">
+                    <div className="result-details-type">
+                      {CapitaliseFirstLetter(mediaSearchResult.Type)}
+                    </div>
+                    <div className="result-details-year">
+                      {MediaYearFormatter(mediaSearchResult.Year)}
+                    </div>
+                  </div>
                 </div>
               </div>
-            </div>
-          );
-        })}
-      </div>
+            );
+          })}
+        </div>
+      ) : (
+        <div className="homepage-results empty">
+          {isLoading ? (
+            <BeatLoader
+              color="rgba(151, 18, 18, 1)"
+              size={20}
+              speedMultiplier={0.5}
+            />
+          ) : (
+            "No Results Found"
+          )}
+        </div>
+      )}
     </div>
   );
 }
